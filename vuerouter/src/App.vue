143717a97d028e3e9718900dca15b0b58f6941e7
ashishugi <ashishkumarguptacse@gmail.com>
2021-01-20 21:17:54 +0530

    vue_router

---
+++ vuerouter/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <nav>
+      <ul>
+        <li> <router-link to="/">Home</router-link></li>
+        <li><router-link to="/about">About</router-link></li>
+        <li><router-link to="/contact">Contact</router-link></li>
+
+      </ul>
+    </nav>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ vuerouter/src/components/User.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        This user component
+        <h1>{{userId}}</h1>
+    </div>
+</template>
+<script>
+export default {
+    name:'User',
+    data(){
+        return {
+            userId:this.$route.params.id
+        }
+    }
+}
+</script>
